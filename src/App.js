621e9d061e353fde262bf0fc8ce9944b26bafe92
bngh <masafumi@DESKTOP-C0S2H4V>
2023-05-19 18:33:40 +0900

    データバインド

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Comp01 from "./Comp01.jsx"
+import Comp02 from "./Comp02.jsx"
 
 function App() {
   return (
@@ -6,6 +7,8 @@ function App() {
       <h2>Reactサンプルコード</h2>
       <h3>#1 HTML出力</h3>
       <Comp01></Comp01>
+      <h3>#2 データバインド</h3>
+      <Comp02></Comp02>
       <hr/>
     </div>
   );
---
+++ src/Comp02.jsx
@@ -0,0 +1,6 @@
+function Comp02() {
+    return(
+        <p>時刻：{new Date().toLocaleTimeString()}</p>
+    );
+}
+export default Comp02;
