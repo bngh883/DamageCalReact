a7072287a12cccfaf8c27c6427e0f8bc00bf1b45
bngh <masafumi@DESKTOP-C0S2H4V>
2023-05-19 19:45:29 +0900

    表示切替

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Comp01 from "./Comp01.jsx"
 import Comp02 from "./Comp02.jsx"
 import Comp03 from "./Comp03.jsx"
-import Comp04 from "./Comp04.jsx";
+import Comp04 from "./Comp04.jsx"
+import Comp05 from "./Comp05.jsx"
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
       <Comp03></Comp03>
       <h3>#4 イベント処理</h3>
       <Comp04></Comp04>
+      <h3>#5 表示切替え</h3>
+      <Comp05></Comp05>
       <hr/>
     </div>
   );
---
+++ src/Comp05.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function Comp05() {
+    const photo = {
+        src: "./logo192.png",
+        alt: "Reactアイコン",
+    };
+
+    const [isShow, setShow] = useState(true);
+
+    return(
+        <div>
+            <button onClick={() => setShow(true)}>表示</button>
+            <button onClick={() => setShow(false)}>非表示</button>
+            {isShow ? 
+                (<p>
+                    <img src={photo.src} alt={photo.alt}/>
+                </p>)
+                : ""
+            }
+        </div>
+    );
+}
