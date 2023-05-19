140e2772277738f05adce7930f890fc6cc9d8fa5
bngh <masafumi@DESKTOP-C0S2H4V>
2023-05-19 19:13:03 +0900

    プロパティバインド

--- src/Comp03.jsx
+++ src/Comp03.jsx
@@ -2,10 +2,11 @@ function Comp03() {
     const photo = {
         src: "./logo192.png",
         alt: "Reactアイコン",
+        width: "100"
     };
 
     return(
-        <img src={photo.src} alt={photo.alt}></img>
+        <img src={photo.src} alt={photo.alt} width={photo.width}/>
     );
 }
 
